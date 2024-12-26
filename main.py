0178d2a56b833a051ccc3c59e22567a3e11f0b18
Salami-Taiwo-Oyekunle <salamitaiwo@yahoo.com>
2024-12-26 18:03:38 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+name='Suzie'
+age=12
+is_student=True
+weight=33.2
+print('name is',name)
+print('data type of name is',type(name))
+print('age is',age)
+print('data type of age is',type(age))
+print('whether i am a student is',is_student)
+print('data type of isstudent is',type(is_student))
+print('weight is',weight)
+print('data type of weight is',type(weight))
+age=str(age)
+print('age is',age)
+print('data type of age is',type(age))
+weight=int(weight)
+print('weight is',weight)
+print('data type of weight is',type(weight))
